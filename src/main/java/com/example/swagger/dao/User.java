ce3b10b204e0395bf254e64489f9894d2587294f
1174959544@qq.com <1174959544@qq.com>
2020-09-22 18:42:58 +0800

    swagger

--- src/main/java/com/example/swagger/dao/User.java
+++ src/main/java/com/example/swagger/dao/User.java
@@ -5,8 +5,8 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel("用户实体类")
 public class User {
-    @ApiModelProperty("用户名")
+    @ApiModelProperty(value = "用户名",position = 1)
     public String username;
-    @ApiModelProperty("密码")
+    @ApiModelProperty(value = "密码",position = 2)
     public String password;
 }
